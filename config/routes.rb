ea3b024fb35263d37a212b0c292ae4b63c147a0c
Dimitar Peychinov <dp86jd@gmail.com>
2013-07-15 09:10:21 +0300

    FIxed homepage.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Diary::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'entries#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
