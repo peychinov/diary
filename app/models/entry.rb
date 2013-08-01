4a636ba6ede2b1ddbb74583d9578829d4b6b1211
Dimitar Peychinov <dp86jd@gmail.com>
2013-08-01 10:31:53 +0300

    Fixed searching by tags.

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -3,29 +3,8 @@ class Entry < ActiveRecord::Base
 
   acts_as_taggable
 
-  include Tire::Model::Search
-  include Tire::Model::Callbacks
-
-  mapping do
-    indexes :tag_names
-    indexes :content
-  end
-
   def self.search(params)
-    per_page = params[:per_page] || 5
-
-    tire.search(load: true, per_page: per_page, page: params[:page] || 1) do
-      query do
-        boolean do
-          must { term :tag_names, params[:tag] } if params[:tag].present?
-        end
-      end
-      sort { by :created_at, 'desc' } if params[:keywords].blank?
-    end
-  end
-
-  def to_indexed_json
-    to_json(methods: [:tag_names])
+    params[:tag].present? ? Entry.tagged_with(params[:tag]) : Entry.all
   end
 
   def tag_names
