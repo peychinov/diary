b5d1670a205088afb69f1562555196a8ba326077
Dimitar Peychinov <dp86jd@gmail.com>
2017-11-26 18:53:38 +0200

    Fix posting the tag_list

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -78,6 +78,6 @@ class EntriesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def entry_params
-      params.require(:entry).permit(:content, :tag_list, :priority, :happened_on, :book, :chapter, :verse)
+      params.require(:entry).permit(:content, { tag_list: [] }, :priority, :happened_on, :book, :chapter, :verse)
     end
 end
